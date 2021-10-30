157620befb1b5452bb5f593e5650b5cbc783fda1
Bonian <robertpan321@gmail.com>
2021-10-30 17:08:15 -0500

    Create Lattice SiO2.py

---
+++ Lattice SiO2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 15:26:12 2021
+
+@author: Bonia
+"""
+"""Unit of the lattice parameters: Angstrom"""
+LiUC = np.array([[0.66667,    0.33333,    0.25821 ],[0.33333,    0.66667,    0.75821]])
+CoUC = np.array([[0.66667,    0.33333,    0.00051 ],[0.33333,    0.66667,    0.50051]])
+OUC = np.array([[0.66667,    0.33333,    0.25821 ],[0.33333,    0.66667,    0.75821]])
+unitcell = 
