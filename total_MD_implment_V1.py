d494b940ec201adc7d605aea6956321bfae3692e
charith44 <93231597+charith44@users.noreply.github.com>
2021-11-23 16:31:55 -0600

    Add files via upload

--- total_MD_implment_V1.py
+++ total_MD_implment_V1.py
@@ -7,9 +7,10 @@ Created on Tue Nov 23 15:15:32 2021
 
 #implement MD Code
 import total_MD_code_V1 as F1
+import Lattice_Graphite as coor
 import numpy as np
 
-def advance(pos, vel, mass, dt, disp, dist, rc, L):
+def advance(pos, vel, mass, dt, disp, dist, rc, L,kvecs):
     """
     advance system according to velocity verlet
 
@@ -27,7 +28,7 @@ def advance(pos, vel, mass, dt, disp, dist, rc, L):
         new positions, new velocities, new displacement table,
         and new distance table
     """
-    accel = (F1.force(disp, dist, rc) + F1.Ewald_force(pos,F1.my_legal_kvecs(N_max,L),alpha,V,L)) / mass
+    accel = (F1.force(disp, dist, rc) + F1.Ewald_force(pos,kvecs,alpha,V,L)) / mass
     #move
     vel_half = vel + 0.5*dt*accel
     pos_new = pos + dt*vel_half
@@ -42,23 +43,27 @@ def advance(pos, vel, mass, dt, disp, dist, rc, L):
 
 #initial Values
 T = 1.0
-L = 4.0
+L = 10.0
 M = 1.0
 cutoff = L/2
 N = 8
 V = L**3
 alpha = (np.pi)*(N/(V**2))**(1/3)
 
-steps = 100
+steps = 1
 timestep = 0.03
 
 N_max = 4
+Karr = F1.my_legal_kvecs(N_max,L)
 
 num_bins = 14
 dr_ = 0.25
 
 #system
-#coordinates = cubic_lattice(4, length)
+coordinates = coor.a
+Qs = np.random.uniform(-1,1,len(coordinates))
+Qs = np.array([Qs]).T
+coordinates = np.append(coordinates,Qs,axis=1)
 velocities = F1.initial_velocities(N, M, T)
 
 #tables required to compute quantities like forces, energies
@@ -83,7 +88,7 @@ veloci = []
 for _ in range(steps):
     coordinates, velocities, displacements, distances = advance(coordinates,\
             velocities, M, timestep, displacements, distances, cutoff,\
-            L)
+            L,K_vecs)
     
     PE.append(F1.potential(distances, cutoff)+F1.Ewald_pot(coordinates,F1.my_legal_kvecs(N_max,L),alpha,V,L))
     PE_EW.append(F1.Ewald_pot(coordinates,F1.my_legal_kvecs(N_max,L),alpha,V,L))
